a48002fdf64dbe112b68031d2a3faeda85288d45
darivagi <57354430+darivagi@users.noreply.github.com>
2022-05-01 12:53:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector(".log-btn").onclick = function () {
+        location.href = "/Users/dashenka/Desktop/web/project site/login.html";
+    };
+
+document.querySelector(".sign-btn").onclick = function () {
+        location.href = "/Users/dashenka/Desktop/web/project site/signup.html";
+    };
