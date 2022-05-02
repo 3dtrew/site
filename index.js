1f579763d95777099eb2d306a68af8db81689436
3dtrew <32934171+3dtrew@users.noreply.github.com>
2022-05-02 13:55:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+document.querySelector(".log-btn").onclick = function () {
+        location.href = "/Users/dashenka/Desktop/project site/login.html";
+    };
+
+document.querySelector(".sign-btn").onclick = function () {
+        location.href = "/Users/dashenka/Desktop/project site/signup.html";
+    };
+document.querySelector(".login-btn").onclick = function () {
+            location.href = "/Users/dashenka/Desktop/project site/userpage.html";
+        };
+document.querySelector(".login-btn").onclick = function () {
+                    location.href = "/Users/dashenka/Desktop/project site/userpage.html";
+                };
+
+
+document.querySelector("body > header")
